0cf25ec68fbdaf67e3cbc9401e8b79621ec9e77b
Sheikh-Sahab <58458615+Sheikh-Sahab@users.noreply.github.com>
2023-04-16 16:13:37 +0100

    Doc Appointment Code

---
+++ DoctorAppointments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManagement
+{
+    public class DoctorAppointments
+    {
+        
+        public void GetAppointments()
+        {
+            string query = "select * from tbl_appointment where app_id = '1065'";
+            Connection objconn = new Connection();
+            objconn.ExecuteQueries(query);
+        }
+    }
+
+  
+}
